da6c73736ae2969e0ed6f5aca2464b3344d09a6b
arsiihere <arsiiahmi@gmail.com>
2022-08-04 02:38:22 +0500

    finish the project

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,15 @@
-exports.getPost = (rqe, res, nex) => {};
+exports.getPost = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "first Post", content: "This is the first post" }],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // creat post in db
+  res.status(201).json({
+    message: "Post created succesfully",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const feedController = require("../controllers/feed");
 
 router.get("/posts", feedController.getPost);
+router.post("/posts", feedController.createPost);
 
 module.exports = router;
